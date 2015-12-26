51f73bdbc18f465a208816f0ade2d7d594b8652d
itagaki <itagaki.shintaro@gmail.com>
2015-12-26 19:45:26 +0900

    1日目終了

--- js/map.js
+++ js/map.js
@@ -1,84 +1,126 @@
-google.maps.event.addDomListener( window, 'load', draw );
+function mapDraw( tomatoArray ) {
+  console.log( 'Start map draw' );
+  draw();
+  // google.maps.event.addDomListener( window, 'load', draw );
 
-var myMap;
-var tomatoArray = [
-  {
-    name: 'ももたろう',
-    lat: 33.559706,
-    lng: 133.531079
-}, {
-    name: 'ももたろう2',
-    lat: 33.900000,
-    lng: 133.000000
-}
- ];
+  var myMap;
+  // var tomatoArray =
+  // [
+  //   {
+  //     name: 'ももたろう',
+  //     lat: 33.559706,
+  //     lng: 133.531079
+  // }, {
+  //     name: 'ももたろう2',
+  //     lat: 33.900000,
+  //     lng: 133.000000
+  // }, {
+  //     name: 'ももたろう3',
+  //     lat: 33.900000,
+  //     lng: 133.900000
+  // }
+  //  ];
 
-function draw() {
-  initialize();
-  overlayTomato();
-}
+  function draw() {
+    initialize();
+    overlayTomato();
+  }
 
-function initialize() {
-  // キャンパスの要素を取得する
-  var canvas = document.getElementById( 'map-canvas' );
+  function initialize() {
+    // キャンパスの要素を取得する
+    var canvas = document.getElementById( 'map-canvas' );
 
-  // 中心の位置座標を指定する
-  var latlng = new google.maps.LatLng( 33.559706, 133.531079 );
+    // 中心の位置座標を指定する
+    var latlng = new google.maps.LatLng( 33.559706, 133.531079 );
 
-  // 地図のオプションを設定する
-  var mapOptions = {
-    zoom: 9, // ズーム値
-    center: latlng, // 中心座標 [latlng]
-  };
+    // 地図のオプションを設定する
+    var mapOptions = {
+      zoom: 12, // ズーム値
+      center: latlng, // 中心座標 [latlng]
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      navigationControl: true, // ナビゲーションレベルコントローラ（true/"SMALL"/"ZOOM_PAN"）
+      mapTypeControl: true, //地図タイプコントローラ (true/"DROPDOWN"）
+      scaleControl: true, //スケールの表示
+      DClickZoom: true, //ダブルクリックでのズーム操作(true:有効, false:無効）
+      scrollwheel: true, //ホイールでのズーム操作(true:有効, false:無効）
+    };
 
-  // [canvas]に、[mapOptions]の内容の、地図のインスタンス([map])を作成する
-  myMap = new google.maps.Map( canvas, mapOptions );
-}
+    // [canvas]に、[mapOptions]の内容の、地図のインスタンス([map])を作成する
+    myMap = new google.maps.Map( canvas, mapOptions );
 
-function overlayTomato() {
-  tomatoArray.forEach( function ( tomato ) {
+    // http://phpjavascriptroom.com/?t=ajax&p=googlemapsapiv3_styling
+    var stylesArray = [
+      {
+        elementType: 'geometry',
+        featureType: 'landscape.natural',
+        stylers: [
+          {
+            visibility: 'simplified'
+          }
+    ]
+  },
+      {
+        featureType: '',
+        // etc...
+  }
+];
+    myMap.setOptions( {
+      styles: stylesArray
+    } );
+  }
+
+  function overlayTomato() {
+    //オーバーレイ設定
+    var overlay = new google.maps.OverlayView(); //OverLayオブジェクトの作成
+
     // D3jsでトマトの画像を表示
-    if ( tomato.lat ) {
-      var overlay = new google.maps.OverlayView(); //OverLayオブジェクトの作成
-      overlay.onAdd = function () {
-        //オーバーレイ設定
-        var layer = d3.select( "#map-canvas" ).append( "div" ).attr( "class", "SvgOverlay" );
-        var svg = layer.append( "svg" );
-
-        // 緯度経度をpx座標に変換
-        var point = this.getProjection().fromLatLngToDivPixel( new google.maps.LatLng( tomato.lat, tomato.lng ) );
-        overlay.draw = function () {
-          // imageを表示
-          svg.selectAll( ".node" )
-            .data( [ point ] ).enter()
-            .append( "image" )
-            .attr( "class", "node" )
-            .attr( "x", function ( d ) {
-              return d.x;
-            } )
-            .attr( "y", function ( d ) {
-              return d.y;
-            } )
-            .attr( "width", 100 )
-            .attr( "height", 100 )
-            .attr( "xlink:href", "img/tomato.jpg" );
-          // ブランド名をテキストで表示
-          svg.selectAll( "text" )
-            .data( [ point ] ).enter()
-            .append( "text" )
-            .attr( "x", function ( d ) {
-              return d.x;
-            } )
-            .attr( "y", function ( d ) {
-              return d.y;
-            } )
-            .attr( "fill", "orange" )
-            .attr( "font-size", "2rem" )
-            .text( tomato.name );
-        }
-        overlay.draw();
-      }
-      overlay.setMap( myMap );
-    }
-  } );
+    overlay.onAdd = function () {
+      var layer = d3.select( "#map-canvas" ).append( "div" ).attr( "class", "SvgOverlay" );
+      var svg = layer.append( "svg" );
+      tomatoArray.forEach( function ( tomato ) {
+        if ( tomato.lat ) {
+          // 緯度経度をpx座標に変換
+          var point = overlay.getProjection().fromLatLngToDivPixel( new google.maps.LatLng( Number( tomato.lat ), Number( tomato.lng ) ) );
+          overlay.draw = function () {
+            // imageを表示
+            svg
+            // .selectAll( ".node" )
+            // .data( [ point ] )
+              .append( "image" )
+              .attr( "class", "node" )
+              .attr( "x", function () {
+                return point.x;
+              } )
+              .attr( "y", function () {
+                return point.y;
+              } )
+              .attr( "width", 50 )
+              .attr( "height", 50 )
+              .attr( "xlink:href", "img/tomato01.png" )
+              .attr( "id", tomato.objectId )
+              .on( "click", function () {
+                alert( tomato.brand );
+              } );
+            // ブランド名をテキストで表示
+            svg
+            // .selectAll( "text" )
+            // .data( [ point ] ).enter()
+              .append( "text" )
+              .attr( "x", function () {
+                return point.x;
+              } )
+              .attr( "y", function () {
+                return point.y;
+              } )
+              .attr( "fill", "red" )
+              .attr( "font-size", "1rem" )
+              .text( tomato.brand );
+          };
+          overlay.draw();
+        } // END of if ( tomato.lat )
+        // overlay.setMap( myMap );
+      } ); // END of forEach
+    }; // END of overlay.onAdd
+    overlay.setMap( myMap );
+  }
 }
--- js/mbaas.js
+++ js/mbaas.js
@@ -1,26 +1,27 @@
 // mobile backendアプリとの連携
-var ncmb = new NCMB( "7d85a8b1dcf0d9fd80dcdd06183e77fb2b5cb913c8abc522182a245ced11afb9", "ffa91e51db0c577ae40223576992e551ccf1e8db73f4f45c061deccd9da9da42" );
+var ncmb = new NCMB( "479af5ec8578bc7590a35c30d4a2a4dcf1877d4d74c29d344044eb7ce542ce15", "f212ac2e2c2a745cc125926872d2c8a9e49b388923110fc446f1020c06975989" );
 
-// TomatoClassの作成
-var TomatoClass = ncmb.DataStore( "TomatoClass" );
-// TomatoClassへの入出力
-TomatoClass.equalTo( "message", "Hello, NCMB!" )
-  .fetchAll()
-  .then( function ( results ) {
-    if ( results[ 0 ] != null ) {
-      alert( results[ 0 ].get( "message" ) );
-    } else {
-      var tomatoClass = new TomatoClass();
-      tomatoClass.set( "message", "Hello, NCMB!" );
-      tomatoClass.save()
-        .then( function () {
-          alert( "message is saved." );
-        } )
-        .catch( function ( err ) {
-          alert( err.text );
-        } );
-    }
-  } )
-  .catch( function ( err ) {
-    alert( err.text );
-  } );
+$( function () {
+  getTomatoArray();
+} );
+
+function getTomatoArray() {
+  var tomatoArray = [];
+  // TomatoClassの作成
+  var TomatoClass = ncmb.DataStore( "tomato" );
+  // TomatoClassへの入出力
+  TomatoClass
+    .fetchAll()
+    .then( function ( results ) {
+      results.forEach( function ( object ) {
+        tomatoArray.push( object );
+      } );
+      console.log( 'Gotten tomatoArray from mBaaS' );
+      console.log( tomatoArray );
+      mapDraw( tomatoArray );
+
+    } )
+    .catch( function ( err ) {
+      alert( err.text );
+    } );
+}
